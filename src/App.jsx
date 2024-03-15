e28cef5242213f6f05e41762211e4c4f18fb23cc
JoSungHyeon <jsh950618@gmail.com>
2024-03-15 14:00:06 +0900

    useCallback commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useRef, useReducer } from 'react'
+import { useState, useRef, useReducer, useCallback } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -44,7 +44,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -53,22 +53,23 @@ function App() {
         content: content,
         date: new Date().getTime(),
       }
-    })
-  }
+    });
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId
-    })
-  }
+    });
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId
-    })
-  }
+    });
+  }, []);
+
   return (
     <div className='App'>
       <Header />
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import './TodoItem.css'
+import { memo } from 'react';
 
 const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
 
@@ -25,4 +26,18 @@ const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
     )
 };
 
-export default TodoItem;
+// 고차 컴포넌트 (HOC)
+// export default memo(TodoItem, (prevProps, nextProps)=>{
+//     // 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+//     // True -> Props 바뀌지 않음 -> 리렌더링 X
+//     // False -> Props 바뀜 -> 리렌더링 O
+
+//     if(prevProps.id !== nextProps.id) return false;
+//     if(prevProps.isDone !== nextProps.isDone) return false;
+//     if(prevProps.content !== nextProps.content) return false;
+//     if(prevProps.date !== nextProps.date) return false;
+
+//     return true;
+// });
+
+export default memo(TodoItem);
